f30a0fff61ef5b619ced612cccd95c78a26850b4
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-01 06:44:27 +0100

    new controller and view files

---
+++ controller.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+COntroller module to coordinate between view and model
+"""
+
+__author__ = 'Mohamed Ouertani'
+
+def Controller():
+    pass
