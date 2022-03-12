58e14fc441e61bb5a788b9812e174b219c3f7dd8
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-12 10:56:18 +0100

    status inside text widget

--- controller.py
+++ controller.py
@@ -44,7 +44,6 @@ class Controller():
             compressed_seq,binary_sequence = self.model.compress_sequence()
             self.view.update_text(f"Binary sequence : {binary_sequence}\n\n"
                                  +f"Compressed sequence : {compressed_seq}")
-            self.view.change_status(compressed_seq)#,"Compressed"
         else:
             self.view.show_warning("Sequence is already compressed")
         
@@ -54,13 +53,12 @@ class Controller():
             decompressed_seq,binary_sequence = self.model.decompress_sequence()
             self.view.update_text(f"Binary sequence : {binary_sequence}\n\n"
                                  +f"Decompressed sequence : {decompressed_seq}")
-            self.view.change_status(decompressed_seq)#,"Uncompressed"
         else:
             self.view.show_warning("Sequence is already decompressed")
 
     def transform_bwt(self):
         if self.model.is_uncompressed():
-            if not self.model.bwt_handler.is_bwt():
+            if not self.model.bwt_handler.input_is_bwt():
                 bwt_generator = self.model.sequence_to_bwt()
                 self.model.update_current_function(bwt_generator)
                 self.view.update_text("""Transform bwt function, Press Next or End to continue""")
@@ -72,7 +70,7 @@ class Controller():
 
     def transform_sequence(self):
         if self.model.is_uncompressed():
-            if self.model.bwt_handler.is_bwt():
+            if self.model.bwt_handler.input_is_bwt():
                 bwt_decoder = self.model.bwt_to_sequence()
                 self.model.update_current_function(bwt_decoder)
                 self.view.update_text("""Transform sequence function, Press Next or End to continue""")
@@ -87,9 +85,8 @@ class Controller():
                 next_value = next(self.model.current_function)
                 self.view.update_text(f" Next step :\n{next_value} ")
             except StopIteration:
-                self.view.update_text("The protocole is finished, please refer to the main menu.")
                 current_sequence = self.model.get_current_sequence()
-                self.view.change_status(current_sequence)
+                self.view.update_text(f"Current sequence : {current_sequence}")
         else:
             self.view.show_warning("No function is chosen yet")
 
@@ -99,9 +96,8 @@ class Controller():
                 last_value = list(self.model.current_function)[-1]
                 self.view.update_text(f" Last step :\n{last_value} ")
             except IndexError:
-                self.view.update_text("The protocole is finished, please refer to the main menu.")
                 current_sequence = self.model.get_current_sequence()
-                self.view.change_status(current_sequence)
+                self.view.update_text(f"Current sequence : {current_sequence}")
         else:
             self.view.show_warning("No function is chosen yet")
 
@@ -117,8 +113,7 @@ class Controller():
         if file_path:
             if self.is_plain_text(file_path):
                 loaded_sequence = self.model.file_loader(file_path,file_name)
-                self.view.change_status(loaded_sequence)
-                self.view.update_text("")
+                self.view.update_text(f"Current sequence : {loaded_sequence}")
             else:
                 self.view.show_warning("wrong file format")
                 #"Sequence longer than 50 :\nTry another file"
