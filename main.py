84c675bb7bdb4a8f592ac518272f2f6c21b94bbc
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-12 20:45:00 +0100

    main outside of package

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Main script to launch the program"""
+
+from __future__ import absolute_import
+from dnashrink.controller  import Controller
+
+def main() -> None:
+    """Launches the program."""
+    Brains = Controller()
+    Brains.launch_view()
+
+if __name__ == "__main__":
+    main()
+    
