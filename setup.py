85b1d9223d2e3ca934e335a87a52c8c5949fa21e
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-12 20:44:11 +0100

    new setup files

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dnashrink-bioinfo',
+    version='0.1.0',
+    description='A Python implementation of the Burrows-Wheeler and Huffman coding algorithms',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Ouertani95/PROJET_ALGO',
+    author='Ouertani-Mohamed',
+    author_email = 'ouertani2006@gmail.com',
+    download_url = 'https://github.com/Ouertani95/PROJET_ALGO/archive/refs/heads/master.zip',
+    packages=setuptools.find_packages(include=['dnashrink', 'dnashrink.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["binaryornot==0.4.4",
+                      "setuptools==45.2.0]"
+                      ],
+    extras_require={
+        'dev': [
+            'pytest >= 6.0.0',
+            'pytest-cov >= 2.10.0',
+            'coveralls >= 2.1.2',
+            'flake8 >= 3.8.0',
+            'mock >= 4.0.0',
+        ]
+    },
+    entry_points={
+        'program_scripts': [
+            'dnashrink=main:main'
+        ]
+    },
+    python_requires='>=3.6',
+)
