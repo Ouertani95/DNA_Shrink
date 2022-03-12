b209dd7f5d5ab4a25ee162e5017e5f75a425ba24
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-12 06:28:07 +0100

    new bash script launcher

---
+++ run_program.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# using the full path of the 'python3' command is not necessary but should used.
+# you can get your full path of the python command by running `which python3`
+/usr/bin/python3 controller.py
