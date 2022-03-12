7b1282cf31016d3b6ebe85b6008594e979814d21
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-12 19:02:34 +0100

    changed main path

--- run_program.sh
+++ run_program.sh
@@ -2,4 +2,4 @@
 
 # using the full path of the 'python3' command is not necessary but should used.
 # you can get your full path of the python command by running `which python3`
-/usr/bin/python3 controller.py
+/usr/bin/python3 ./dnashrink/main.py
