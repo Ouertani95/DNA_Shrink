cf6d034388fc6e068a162c1851b0611c4d8490da
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-13 17:01:21 +0100

    Commented model module

--- dnashrink/model.py
+++ dnashrink/model.py
@@ -2,71 +2,138 @@
 # -*- coding: utf-8 -*-
 
 """
-Model module to manipulate the data
+Model module part of the MVC architechture that represents the component 
+interacting and manipulating the data directly under the commands of the Controller
 """
 
 __author__ = 'Mohamed Ouertani'
 
 import os
+from typing import Generator
 from dnashrink.bwt import Bwt
 from dnashrink.huffman import Huffman
 
 
 class Model():
 
-    """Model class"""
+    """
+    Model class to manipulate all the data in the program
 
-    def __init__(self,controller):
+    Attributes
+    ----------
+    controller : Controller
+        Controller object that calls the model
+    bwt_handler : Bwt
+        Bwt object to be called for Burrows-Wheeler transform 
+        and reverse transform on DNA sequences
+    huffman_handler : Huffman
+        Huffman object to be called for Huffman compression and 
+        decompression of DNA sequences
+    decoding_dict : dict
+        Dictionnary necessary for Decompression of DNA sequences
+    current_file : str
+        Currently used file name without extension used for saving files
+    current_sequence : str
+        Sequence being treated inside the program
+    current_function : method
+        Function being used to transform the current_sequence
+    bwt_status : bool
+        Current state of the sequence : BWT or Normal
+    """
+
+    def __init__(self,controller) -> None:
+        """
+        Class method for Creation of interface and all the widgets inside
+
+        Parameters
+        -----------
+        controller : Controller
+            Controller Object coordinating between the model and view
+
+        Returns:
+        ----------
+        None
+        """
         self.controller = controller
-
         self.bwt_handler = None
         self.huffman_handler = None
-
-        self.input_sequence = None
         self.decoding_dict = None
-
         self.current_file = None
         self.current_sequence = None
         self.current_function = None
-
         self.bwt_status = None
 
 
+    def file_loader(self,input_file,file_name) -> str:
+        """
+        Class method to load file into model object and update attributes
 
-    def file_loader(self,input_file,file_name):
-        """Loads file into model object and updates attributes"""
-        self.input_sequence = self.sequence_extractor(input_file)
+        Returns:
+        ----------
+        current_sequence : str
+            Sequence being transformed inside the program
+        """
+        #Load new file name
         self.current_file = file_name
-        print(self.current_file)
-        self.current_sequence = self.input_sequence
+        #Extract sequence from file using sequence_extractor method
+        self.current_sequence = self.sequence_extractor(input_file)
+        #Conditional update of attributes
         if self.is_uncompressed():
-            self.bwt_handler = Bwt(self.input_sequence)
+            self.bwt_handler = Bwt(self.current_sequence)
             if self.bwt_handler.input_is_bwt():
                 self.bwt_status = True
             else:
                 self.bwt_status = False
         else:
             self.bwt_handler = None
-        self.huffman_handler = Huffman(self.input_sequence,self.decoding_dict)
+        self.huffman_handler = Huffman(self.current_sequence,self.decoding_dict)
         return self.current_sequence
 
 
-    def sequence_extractor(self,input_file):
-        """Reads a sequence file and returns the sequence ready to be encoded"""
+    def sequence_extractor(self,input_file) -> str: #TODO : modify opening process
+        """
+        Class method to read a sequence file and extract the sequence inside
 
+        Parameters
+        -----------
+        input_file : str
+            path of file from which the sequence will be extracted
+
+        Returns:
+        ----------
+        raw_sequence : str
+            Sequence extracted from the input file
+        """
+        #Open input_file in read mode
         with open(input_file,"r") as file_input:
+            #Create a list of strings representing all the lines inside the file
             all_lines = file_input.readlines()
-            raw_sequence = all_lines[0]
+        #Verify is file contains string_dictionnary or not
+        if ":" in all_lines[-1] and "," in all_lines[-1]:
+            #Recover the full DNA sequence
+            raw_sequence = "".join(all_lines[0:len(all_lines)-1])
+            #Recover the last line representing the decoding_dict in string format
+            string_dict = all_lines[-1]
+            #Convert dictionnary from string format to original format
+            self.string_to_dict(string_dict)
+        else:
+            #Recover the full DNA sequence
+            raw_sequence = "".join(all_lines)
+        #Clean the sequence by removing spaces and backlines
         raw_sequence = raw_sequence.replace(" ","")
         raw_sequence = raw_sequence.replace("\n","")
-        if len(all_lines)>1:
-            string_dict = all_lines[1]
-            self.string_to_dict(string_dict)
-        print("input sequence : \n",raw_sequence)
+        
         return raw_sequence
 
-    def is_uncompressed(self):
-        """Verifies if a sequence is uncompressed"""
+    def is_uncompressed(self) -> bool:
+        """
+        Class method that verifies if a sequence is uncompressed
+
+        Returns:
+        ----------
+        uncompressed : bool
+            Verification result of compression status of self.current_sequence 
+        """
         uncompressed = True
         for char in self.current_sequence:
             if char not in ["A","T","G","C","N","$"]:
@@ -75,46 +142,101 @@ class Model():
         return uncompressed
 
 
-    def compress_sequence(self):
-        """Compresses the current sequence"""
+    def compress_sequence(self) -> str:
+        """
+        Class method that compresses the current sequence
+
+        Returns:
+        ----------
+        current_sequence : str
+            Final Char sequence after compression
+        binary_sequence : str
+            Intermediate binary sequence used for compression
+        """
+        #Transform current_sequence to binary_sequence
         binary_sequence = self.huffman_handler.sequence_to_binary()
+        #Transfotm binary_sequence to Char sequence
         huffman_sequence, decoding_dict = self.huffman_handler.binary_to_char()
+        #Update model attributes
         self.current_sequence = huffman_sequence
         self.decoding_dict = decoding_dict
         return self.current_sequence , binary_sequence
 
-    def decompress_sequence(self):
-        """Decompresses the current sequence"""
+    def decompress_sequence(self) -> str:
+        """
+        Class method that decompresses the current sequence
+        
+        Returns:
+        ----------
+        current_sequence : str
+            Final Char sequence after decompression
+        binary_sequence : str
+            Intermediate binary sequence used for decompression
+        """
+        #Transform current_sequence to binary_sequence
         binary_sequence = self.huffman_handler.char_to_binary()
+        #Transform binary_sequence to original_sequence
         decompressed_sequence = self.huffman_handler.binary_to_sequence()
+        #Update bwt_handler with new sequence
         self.bwt_handler = Bwt(decompressed_sequence)
+        #Update current_sequence
         self.current_sequence = decompressed_sequence
         return self.current_sequence,binary_sequence
 
-    def sequence_to_bwt(self):
-        """Transforms normal sequence to bwt"""
+    def sequence_to_bwt(self) -> Generator:
+        """
+        Class method that transforms normal_sequence to bwt_sequence
+
+        Returns:
+        ----------
+        Generator :
+            When called returns the results of bwt transformation step by steps
+        """
+        #Return bwt generation steps one by one
         yield from self.bwt_handler.bwt_generator()
+        #Recover final result of transformation directly
         bwt_sequence = list(self.bwt_handler.bwt_generator())[-1]
+        #Update model attribuutes using new bwt_sequence
         self.current_sequence = bwt_sequence
         self.huffman_handler = Huffman(bwt_sequence)
         self.bwt_handler = Bwt(bwt_sequence)
         self.bwt_status = True
 
-    def bwt_to_sequence(self):
-        """Transforms bwt to normal sequence"""
+    def bwt_to_sequence(self) -> Generator:
+        """
+        Class method that transforms bwt_sequence to normal_sequence
+
+        Returns:
+        ----------
+        Generator :
+            When called returns the results of bwt reverse transformation step by steps
+        """
+        #Return reverse bwt transformation steps one by one
         yield from self.bwt_handler.bwt_decoder()
+        #Recover last result of bwt transformation directly
         original_sequence = list(self.bwt_handler.bwt_decoder())[-1]
+        #Update model attribuutes using new normal_sequence
         self.current_sequence = original_sequence
         self.huffman_handler = Huffman(original_sequence)
         self.bwt_handler = Bwt(original_sequence)
         self.bwt_status = False
 
 
-    def save_file(self):
-        """Save the current sequence into .txt file"""
+    def save_file(self) -> str:
+        """
+        Class method that saves the current sequence into .txt file
+
+        Returns:
+        ----------
+        file_name : str
+            Name of the file that was saved in data directory    
+        """
+        #verify if data directory / if not create it
         if not os.path.exists("./data"):
             os.mkdir("./data")
+        #Verify if sequence is compressed and save file accordingly
         if self.is_uncompressed():
+            #Verify is sequence is bwt or not
             if "$" in self.current_sequence:
                 file_name = f"{self.current_file}_bwt.txt"
                 with open (f"./data/{file_name}","w") as bwt_output:
@@ -124,6 +246,7 @@ class Model():
                 with open (f"./data/{file_name}","w") as original_output:
                     original_output.write(self.current_sequence)
         else:
+            #Verify is sequence was bwt or not before compression
             if self.bwt_status:
                 file_name = f"{self.current_file}_bwt_huffman.txt"
             else:
@@ -134,30 +257,77 @@ class Model():
         return file_name
 
 
-    def update_current_function(self,function):
-        """Update the current function arrtibute"""
+    def update_current_function(self,function) -> None:
+        """
+        Class method to update the current_function arrtibute
+        This attribute is useful for the step_by_step method in Controller
+
+        Parameters
+        -----------
+        function : method
+            Current function being used in view to display results
+
+        Returns:
+        ----------
+        None
+        """
         self.current_function = function
 
 
-    def get_current_sequence(self):
-        """Gets the current sequence"""
+    def get_current_sequence(self) -> str:
+        """
+        Class method used to get the current sequence
+        
+        Returns:
+        ----------
+        current_sequence : str
+            Actual sequence being transformed in the program
+        """
         return self.current_sequence
 
 
-    def dict_to_string(self):
-        """Transforms a dictionnary into a string for the saving process"""
+    def dict_to_string(self) -> str:
+        """
+        Class method that transforms a decoding_dict into a string for the saving process
+
+        Input
+        ----------
+        decoding_dict : dict
+            Dictionnary used for the decompression process
+
+        Returns:
+        ----------
+        string_dict : str
+            Decoding_dict transformed into string format for lighter save
+        """
         string_dict = ""
         for i,j in self.decoding_dict.items():
             string_dict += f"{i}:{j},"
         return string_dict
 
 
-    def string_to_dict(self,string_dict):
-        """Converts a string to a dictionnary to decompress the sequence"""
+    def string_to_dict(self,string_dict) -> None:
+        """
+        Class method that converts a decoding_dict from string 
+        format to a dictionnary for the decompression of the sequence
+
+        Parameters
+        -----------
+        string_dict : str
+            Decoding_dict in string format to be converted into a dictionnary
+        
+        Returns:
+        ----------
+        None
+        """
+        #Split the string using , separator
         dict_elements = string_dict.split(",")
+        #Remove the last , :
         dict_elements = dict_elements[:-1]
-        print(dict_elements)
         self.decoding_dict = {}
+        #Fill the dictionnary with the original values
         for i in dict_elements:
+            #Split key and value using : separator
             temp_items = i.split(":")
+            #Fill the dictionnary
             self.decoding_dict[temp_items[0]]=temp_items[1]
