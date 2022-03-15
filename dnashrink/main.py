10e4a1edae29c0cc77a9c0f7f73690e926fb3e87
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-15 05:19:58 +0100

    new main for dnashrink package

---
+++ dnashrink/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Main script to launch the program"""
+
+__author__ = 'Mohamed Ouertani'
+
+from dnashrink.controller  import Controller
+
+def main() -> None:
+    """
+    main function used to launch the program
+
+    Returns:
+    ----------
+    None
+    """
+    #Initiate Controller Object
+    coordinator = Controller()
+    #Launch the GUI
+    coordinator.launch_view()
+
+#Execute main function
+if __name__ == "__main__":
+    main()
