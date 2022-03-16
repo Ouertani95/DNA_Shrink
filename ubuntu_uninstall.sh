1547984b640a462ea6cbb19816dd3906f76e8710
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-03-16 08:20:54 +0100

    global uninstall

--- ubuntu_uninstall.sh
+++ ubuntu_uninstall.sh
@@ -5,5 +5,5 @@ sudo rm dnashrink_bioinfo-0.1.0-py3.8.egg easy-install.pth
 cd /usr/local/bin
 sudo rm easy_install dnashrink
 cd
-cd Desktop/S2_Master/ALGO/PROJET_ALGO/
+cd ~/Desktop/OUERTANI/PROJET_ALGO
 sudo rm -r build/ dist/ dnashrink_bioinfo.egg-info/
